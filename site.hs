7534e807a364b176569970effa32f10773d70c7d
Anders Claesson <anders.claesson@gmail.com>
2013-07-02 23:53:54 +0100

    Genrate bare-bones index.html

---
+++ site.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Data.Monoid (mappend)
+import Hakyll
+
+main :: IO ()
+main = hakyll $ do
+  match ("images/*.png" .||. "images/*.ico" .||. "fonts/*") $ do
+    route   idRoute
+    compile copyFileCompiler
+
+  match "css/*" $ do
+    route   idRoute
+    compile compressCssCompiler
+
+  match "templates/*" $ compile templateCompiler
+
+  match "index.md" $ do
+     route   $ setExtension "html"
+     compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
